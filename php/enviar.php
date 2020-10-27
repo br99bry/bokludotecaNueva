dfbe1b47a1a1742906d55ff3197c24534264cbde
Bryan <bryanblack1999@gmail.com>
2020-10-26 23:15:25 -0600

    probando contact form

---
+++ php/enviar.php
@@ -0,0 +1,11 @@
+<?php
+
+  $destino="bryan.a@kuper.mx";
+  $nombre=$_POST["nombre"];
+  $apellidos=$_POST["apellidos"];
+  $telefono=$_POST["telefono"];
+  $correo=$_POST["correo"];
+  $contenido="Nombre: " . $nombre . "\nApellidos: " . $apellidos . "\nCorreo: " . $correo . "\nTelefono: " . $telefono;
+  mail($destino,"Visitante Bokludoteca", $contenido);
+  header("Location:../index.html");
+?>
