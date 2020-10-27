f117b21e95ddaadbaf720928ba106639298ae6c5
Bryan <bryanblack1999@gmail.com>
2020-10-27 15:24:11 -0600

    trabajando seccion de paqetes para niños en fiestas page

---
+++ scripts/fiestas.js
@@ -0,0 +1,13 @@
+const burgerMenu = document.getElementById('burgerMenu');
+const menu = document.getElementById('menu');
+const closeMenu = document.getElementById('closeMenu');
+
+burgerMenu.addEventListener('click', () => {
+  menu.classList.add('menu-is-active');
+
+})
+
+closeMenu.addEventListener('click', () => {
+  menu.classList.remove('menu-is-active')
+})
+
